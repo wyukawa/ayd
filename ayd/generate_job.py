23cc1efae65508faddf688cc6e787159fcc0e9a2
wyukawa <wyukawa@gmail.com>
2015-07-30 10:24:50 +0900

    rename

--- ayd/generate_job.py
+++ ayd/generate_job.py
@@ -14,32 +14,32 @@ class Generate_Job(Command):
 
     def get_parser(self, prog_name):
         parser = super(Generate_Job, self).get_parser(prog_name)
-        parser.add_argument('--jobyaml', required=True)
+        parser.add_argument('--flowyaml', required=True)
         parser.add_argument('--outputdir', required=True)
         return parser
 
     def take_action(self, parsed_args):
-        jobyaml = parsed_args.jobyaml
-        if os.path.exists(jobyaml) == False:
-            raise Exception("%s doesn't exist" % (jobyaml))
+        flowyaml = parsed_args.flowyaml
+        if os.path.exists(flowyaml) == False:
+            raise Exception("%s doesn't exist" % (flowyaml))
 
         outputdir = parsed_args.outputdir
         if os.path.exists(outputdir):
             raise Exception("%s already exists" % (outputdir))
         os.mkdir(outputdir)
 
-        with open(jobyaml, "r") as f:
+        with open(flowyaml, "r") as f:
             y = yaml.load(f)
-            for flow_name, flow_content in y.items():
-                self.log.debug("flow_name=%s, flow_content=%s" % (flow_name, flow_content))
-                flow_dir = os.path.join(outputdir, flow_name)
-                os.mkdir(flow_dir)
-                job_file_name = flow_name + ".job"
-                job_file_path = os.path.join(flow_dir, job_file_name)
+            for job_name, job_content in y.items():
+                self.log.debug("job_name=%s, job_content=%s" % (job_name, job_content))
+                job_dir = os.path.join(outputdir, job_name)
+                os.mkdir(job_dir)
+                job_file_name = job_name + ".job"
+                job_file_path = os.path.join(job_dir, job_file_name)
                 with open(job_file_path, "w") as o:
-                    for k, v in flow_content.items():
+                    for k, v in job_content.items():
                         if v is None:
-                            raise Exception("%s at %s has no value" % (k, flow_name))
+                            raise Exception("%s at %s has no value" % (k, job_name))
                         if k == "type" or k == "dependencies" or k == "retries" or k == "retry.backoff" or k[0:7] == "command":
                             o.write(k + "=" + str(v))
                             o.write("\n")
