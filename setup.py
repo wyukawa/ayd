a8c0e11cda3fb9e067549811ae372503b24fa81d
wyukawa <wyukawa@gmail.com>
2015-08-06 10:05:51 +0900

    bump to 0.0.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='ayd',
-    version='0.0.6',
+    version='0.0.7',
     description='generate Azkaban Flow',
     long_description=open('README.md').read(),
     author='wyukawa',
